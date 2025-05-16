9f0eb8869795cecee5e2c62302ba7870931451b3
Leonel <leonel.carro94@gmail.com>
2025-05-15 21:18:21 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace trabajo_integrador_gym_unaj_cdp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+
+    }
+}
