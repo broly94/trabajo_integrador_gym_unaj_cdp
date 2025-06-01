334c4fd8506f12232ff5fa52e32f1448455d154f
Leonel <leonel.carro94@gmail.com>
2025-06-01 00:23:43 -0300

    Proyecto terminado, se asignaron colores y casting de usuarios

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
 
         static void Main(string[] args)
         {
+            Welcome();
             Menu();
         }
         public static String Welcome()
@@ -18,13 +19,9 @@
         public static void Menu()
         {
 
-            Welcome();
+            int option = 0;
 
-            bool exit = false;
-
-            int option;
-
-            while (!exit)
+            while (option != 9)
             {
                 //Recorre el bucle hasta que exit sea lo contrario a su valor
                 //En este caso exit es falso, entonces cuando exit sea verdadero, sale del bucle
@@ -36,14 +33,18 @@
                 Console.WriteLine("4. Mostrar clientes fuera del rango saludable (IMC).");
                 Console.WriteLine("5. Buscar cliente por numero de socio.");
                 Console.WriteLine("6. Promedio de edad de los clientes.");
-                Console.WriteLine("7. Salir");
-                Console.WriteLine("8. Mostrar todos los usuarios");
+                Console.WriteLine("7. Mostrar todos los usuarios");
+                Console.WriteLine("8. Cargar usuarios ficticios");
+                Console.WriteLine("9. Salir");
 
                 string input = Console.ReadLine();
 
                 if (!int.TryParse(input, out option))
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Ingrese un numero que este en el menú.");
+                    Console.ResetColor();
+                    continue;
                 }
                 else
                 {
@@ -76,20 +77,25 @@
                             ShowAgeAverageClient();
                             break;
                         case 7:
-                            exit = Exit();
-                            Console.WriteLine("Bye...");
-                            break;
-                        case 8:
                             ShowAllUsers();
                             break;
+                        case 8:
+                            Console.Clear();
+                            CastingUsers();
+                            break;
+                        case 9:
+                            Console.WriteLine("Bye...");
+                            break;
                         default:
-                            Console.WriteLine("Opcion no reconocida...");
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ingrese un numero que este en el menú.");
+                            Console.ResetColor();
                             break;
 
                     }
+                    Console.ResetColor();
                 }
-
-
             }
         }
 
@@ -173,19 +179,22 @@
             double Weight = 0.0;
             double Height = 0.0;
 
+            Console.ForegroundColor = ConsoleColor.Green;
+
             for (int i = 0; i < ContainerUser.Length; i++)
             {
                 if (ContainerUser[i] != null)
                 {
-                    object[] User = (object[])ContainerUser[i];
-
-                    Name = Convert.ToString(User[0]);
-                    Weight = Convert.ToInt64(User[6]);
-                    Height = Convert.ToDouble(User[7]) * Convert.ToDouble(User[7]);
+                    Name = Convert.ToString(ContainerUser[i][0]);
+                    Weight = Convert.ToInt64(ContainerUser[i][6]);
+                    Height = Convert.ToDouble(ContainerUser[i][7]) * Convert.ToDouble(ContainerUser[i][7]);
                     IMC = Math.Round((Weight / Height) * 10000, 2);
                     Console.WriteLine($"El/La cliente/a {Name} tiene un IMC de: {IMC}");
                 }
             }
+
+            Console.WriteLine("\n");
+            Console.ResetColor();
 
         }
 
@@ -194,26 +203,31 @@
             string LevelActivity = "";
             string Client = "";
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Ingrese que nivel de actividad quiere visualizar...");
             Console.WriteLine("b = (Bajo), m = (Medio), a = (Alto)");
+            Console.ResetColor();
+
             char Letter = char.Parse(Console.ReadLine());
 
+            Console.ForegroundColor = ConsoleColor.Green;
             for (int i = 0; i < ContainerUser.Length; i++)
             {
                 if (ContainerUser[i] != null)
                 {
-                    object[] User = (object[])ContainerUser[i];
 
-                    if (Letter == Convert.ToChar(User[8].ToString().ToLower()[0]))
+                    if (Letter == Convert.ToChar(ContainerUser[i][8].ToString().ToLower()[0]))
                     {
-                        LevelActivity = User[8].ToString();
-                        Client = User[0].ToString();
-                        Console.WriteLine($"El/La cliente/a {Client} tiene un nivel de actividad {User[8]}");
-                        break;
+                        LevelActivity = ContainerUser[i][8].ToString();
+                        Client = ContainerUser[i][0].ToString();
+                        Console.WriteLine($"El/La cliente/a {Client} tiene un nivel de actividad {ContainerUser[i][8]}");
                     }
-                   
+
+
                 }
             }
+            Console.WriteLine("\n");
+            Console.ResetColor();
         }
 
         public static void GetImcByRangeForHealthy()
@@ -224,19 +238,15 @@
             double Weight = 0.0;
             double Height = 0.0;
 
+            Console.ForegroundColor = ConsoleColor.Red;
             for (int i = 0; i < ContainerUser.Length; i++)
             {
                 if (ContainerUser[i] != null)
                 {
-                    object[] User = (object[])ContainerUser[i];
-
-                    for (int j = 0; j < User.Length; j++)
-                    {
-                        Name = Convert.ToString(User[0]);
-                        Weight = Convert.ToInt64(User[6]);
-                        Height = Convert.ToDouble(User[7]) * Convert.ToDouble(User[7]);
-                        IMC = Math.Round((Weight / Height) * 10000, 2);
-                    }
+                    Name = Convert.ToString(ContainerUser[i][0]);
+                    Weight = Convert.ToInt64(ContainerUser[i][6]);
+                    Height = Convert.ToDouble(ContainerUser[i][7]) * Convert.ToDouble(ContainerUser[i][7]);
+                    IMC = Math.Round((Weight / Height) * 10000, 2);
                     if (IMC < 18.5 || IMC > 24.9)
                     {
                         Console.WriteLine($"El/La cliente/a {Name} esta fuera del rango saludable -> IMC: {IMC}");
@@ -244,6 +254,8 @@
                 }
 
             }
+            Console.WriteLine("\n");
+            Console.ResetColor();
         }
 
         public static void ShowClientByMemberShip(int NumberMembership)
@@ -254,22 +266,22 @@
                 if (ContainerUser[i] != null)
                 {
 
-                    object[] User = (object[])ContainerUser[i];
-
-                    if (int.Parse(User[3].ToString()) == NumberMembership)
+                    if (int.Parse(ContainerUser[i][3].ToString()) == NumberMembership)
                     {
 
                         Console.Clear();
-                        Console.WriteLine($"=== Cliente Número: {User[3]} ===");
-                        Console.WriteLine($"Nombre: {User[0]}");
-                        Console.WriteLine($"Apellido: {User[1]}");
-                        Console.WriteLine($"Dni: {User[2]}");
-                        Console.WriteLine($"Email: {User[4]}");
-                        Console.WriteLine($"Edad: {User[5]}");
-                        Console.WriteLine($"Peso Actual (En KG): {User[6]}");
-                        Console.WriteLine($"Altura (en CM): {User[7]}");
-                        Console.WriteLine($"Nivel de actividad: {User[8]}");
-                        Console.WriteLine("==========");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine($"=== Cliente Número: {ContainerUser[i][3]} ===");
+                        Console.WriteLine($"Nombre: {ContainerUser[i][0]}");
+                        Console.WriteLine($"Apellido: {ContainerUser[i][1]}");
+                        Console.WriteLine($"Dni: {ContainerUser[i][2]}");
+                        Console.WriteLine($"Email: {ContainerUser[i][4]}");
+                        Console.WriteLine($"Edad: {ContainerUser[i][5]}");
+                        Console.WriteLine($"Peso Actual (En KG): {ContainerUser[i][6]}");
+                        Console.WriteLine($"Altura (en CM): {ContainerUser[i][7]}");
+                        Console.WriteLine($"Nivel de actividad: {ContainerUser[i][8]}");
+                        Console.WriteLine("==================================");
+                        Console.ResetColor();
                         Console.WriteLine("\n");
 
                     }
@@ -289,9 +301,7 @@
                 if (ContainerUser[i] != null)
                 {
 
-                    object[] User = (object[])ContainerUser[i];
-
-                    SumAge += Convert.ToInt32(User[5]);
+                    SumAge += Convert.ToInt32(ContainerUser[i][5]);
 
                     CounterAge++;
 
@@ -302,24 +312,25 @@
             {
                 double Average = Convert.ToInt32(SumAge / CounterAge);
                 Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine($"El primedio de edad es: {Average}");
+                Console.ResetColor();
             }
             else
             {
                 Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("No hay clientes registrados");
+                Console.ResetColor();
             }
 
-        }
-
-        public static bool Exit()
-        {
-            return true;
         }
 
         public static void ShowAllUsers()
         {
             Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
 
             for (int i = 0; i < ContainerUser.Length; i++)
             {
@@ -327,21 +338,55 @@
                 {
                     Console.WriteLine($"###Cliente numero: {i + 1}####");
 
-                    object[] User = (object[])ContainerUser[i];
+                    Console.WriteLine($"{Keys[0]} : {ContainerUser[i][0]}");
+                    Console.WriteLine($"{Keys[1]} : {ContainerUser[i][1]}");
+                    Console.WriteLine($"{Keys[2]} : {ContainerUser[i][2]}");
+                    Console.WriteLine($"{Keys[3]} : {ContainerUser[i][3]}");
+                    Console.WriteLine($"{Keys[4]} : {ContainerUser[i][4]}");
+                    Console.WriteLine($"{Keys[5]} : {ContainerUser[i][5]}");
+                    Console.WriteLine($"{Keys[6]} : {ContainerUser[i][6]}");
+                    Console.WriteLine($"{Keys[7]} : {ContainerUser[i][7]}");
+                    Console.WriteLine($"{Keys[8]} : {ContainerUser[i][8]}");
 
-                    for (int j = 0; j < User.Length; j++)
-                    {
-                        Console.WriteLine($"{Keys[j]} : {User[j]}");
-
-                    }
                     Console.WriteLine($"=================================\n");
                 }
             }
+
+            Console.ResetColor();
         }
 
+        public static void CastingUsers()
+        {
+            for (int i = 0; i < UsersCast().Length; i++)
+            {
 
+                if (ContainerUser[i] == null)
+                {
+                    ContainerUser[i] = UsersCast()[i];
+                }
 
+            }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Usuarios Creados");
+            Console.ResetColor();
+        }
 
+        public static object[][] UsersCast()
+        {
+            return [
+                [ "Juan", "Pérez", 12345678, 1, "juan.perez@example.com", 25, 70.5, 175, "Medio" ],
+                [ "María", "Gómez", 23456789, 2, "maria.gomez@example.com", 32, 60.2, 160, "Alto" ],
+                [ "Luis", "Martínez", 34567890, 3, "luis.martinez@example.com", 40, 80.0, 180, "Bajo" ],
+                [ "Ana", "López", 45678901, 4, "ana.lopez@example.com", 28, 55.0, 165, "Medio" ],
+                [ "Carlos", "Rodríguez", 56789012, 5, "carlos.rodriguez@example.com", 35, 90.3, 185, "Alto" ],
+                [ "Lucía", "Fernández", 67890123, 6, "lucia.fernandez@example.com", 22, 50.8, 158, "Medio" ],
+                [ "Pedro", "González", 78901234, 7, "pedro.gonzalez@example.com", 30, 75.0, 178, "Bajo" ],
+                [ "Sofía", "Ramírez", 89012345, 8, "sofia.ramirez@example.com", 27, 62.4, 162, "Alto" ],
+                [ "Diego", "Torres", 90123456, 9, "diego.torres@example.com", 45, 85.6, 172, "Medio" ],
+                [ "Valentina", "Sánchez", 11223344, 10, "valentina.sanchez@example.com", 19, 54.3, 167, "Alto" ]
+            ];
+        }
 
     }
 }
